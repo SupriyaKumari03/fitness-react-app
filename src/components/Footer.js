0bba87ff3bf40a89f671b52e5262de6b698b56e5
Supriya Kumari <supriyakumari82982@gmail.com>
2023-12-23 15:18:39 +0530

    first commit

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Box, Typography } from '@mui/material';
+import { Box } from '@mui/material';
 // import Logo from '../assets/images/Logo-1.png';
 
 const Footer = () => (
@@ -11,7 +11,7 @@ const Footer = () => (
     <lottie-player src="https://lottie.host/735fd48a-3fd2-4da8-9389-9002a3ccdc30/tuoXxmZkG0.json" background="##FFFFFF" speed="1" loop autoplay direction="1" mode="normal">Fitness Wonder</lottie-player>
     <div className="fitness">Fitness Wonder</div>
     </div>
-    <Typography variant="h5" sx={{ fontSize: { lg: '28px', xs: '20px' } }} mt="41px" textAlign="center" pb="40px">Made with ❤️ by JavaScript Mastery</Typography>
+    {/* <Typography variant="h5" sx={{ fontSize: { lg: '28px', xs: '20px' } }} mt="41px" textAlign="center" pb="40px"></Typography> */}
   </Box>
 );
 
