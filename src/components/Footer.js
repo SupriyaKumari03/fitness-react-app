c4394ce009ec17c9fc70832cbbea069ae30c93a9
Supriya Kumari <supriyakumari82982@gmail.com>
2024-01-05 15:20:31 +0530

    update

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -8,8 +8,8 @@ const Footer = () => (
        <img src={Logo} alt="logo" style={{ width: '200px', height: '41px' }}/>
     </Stack> */}
     <div className="lotties">
-    <lottie-player src="https://lottie.host/735fd48a-3fd2-4da8-9389-9002a3ccdc30/tuoXxmZkG0.json" background="##FFFFFF" speed="1" loop autoplay direction="1" mode="normal">Fitness Wonder</lottie-player>
-    <div className="fitness">Fitness Wonder</div>
+      <lottie-player src="https://lottie.host/735fd48a-3fd2-4da8-9389-9002a3ccdc30/tuoXxmZkG0.json" background="##FFFFFF" speed="1" loop autoplay direction="1" mode="normal">Fitness Wonder</lottie-player>
+      <div className="fitness">Fitness Wonder</div>
     </div>
     {/* <Typography variant="h5" sx={{ fontSize: { lg: '28px', xs: '20px' } }} mt="41px" textAlign="center" pb="40px"></Typography> */}
   </Box>
